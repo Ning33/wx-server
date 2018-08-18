bba591afce75640c4183f3aec3f88f1d2265fd8f
FuQiang <315458488@qq.com>
2018-08-19 00:08:21 +0800

    增加获取request请求中Json格式的参数 Utuils类

--- src/main/java/cn/hnisi/wx/server/filter/MyIntercepter.java
+++ src/main/java/cn/hnisi/wx/server/filter/MyIntercepter.java
@@ -2,13 +2,14 @@ package cn.hnisi.wx.server.filter;
 
 import cn.hnisi.wx.core.exception.AppException;
 import cn.hnisi.wx.core.io.ResponseStatus;
-import cn.hnisi.wx.core.utils.JsonUtil;
+import cn.hnisi.wx.core.utils.GetRequestJsonUtils;
 import cn.hnisi.wx.server.person.dao.PersonDAO;
 import cn.hnisi.wx.server.person.model.Person;
 import cn.hnisi.wx.server.service.dao.OrderDAO;
 import cn.hnisi.wx.server.service.model.Order;
 import cn.hnisi.wx.server.service.navigation.dao.ServiceItemDAO;
 import cn.hnisi.wx.server.validateface.ValidateFaceService;
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
@@ -37,43 +38,48 @@ public class MyIntercepter implements HandlerInterceptor{
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.info("-----------------开始进入请求地址拦截-----------------");
+        //获取post请求中的参数
+        JSONObject json = GetRequestJsonUtils.getRequestJsonObject(request);
         String serviceName; //服务名
         String personid;    //参保人id
+        Order order ;        //订单
+        Person person = null;      //参保人
         try{
             //截取url路径,获取服务业务名 service_name
             int preNum = request.getRequestURL().indexOf("service");
             int subNum = request.getRequestURL().indexOf("/",preNum+8);
             serviceName = request.getRequestURL().substring(preNum+8,subNum);
+            //查询安全级别 或 判断有无此事项
+            String securityLevel = serviceItemDAO.queryByServiceName(serviceName);
+            if(!StringUtils.isEmpty(securityLevel) && securityLevel.equals("3")){ //需要人脸识别的事项
+                //判断用户是否已经人脸识别
+                personid = json.getString("personid");
+                //如果参数中没有personid 则试用orderno查询personid
+                if(StringUtils.isEmpty(personid)){
+                    String orderno = request.getParameter("orderno");
+                    //如果orderno也没有获取到, 则抛出异常
+                    if(StringUtils.isEmpty(orderno)){
+                        throw new AppException(ResponseStatus.DATA_VALIDATE_EXCEPTION,"数据为空");
+                    }
+                    order = orderDAO.queryByOrderno(orderno);
+                    if(order == null){
+                        throw new AppException(ResponseStatus.UNKNOWN_ERROR);
+                    }
+                    personid = order.getPersonid();
+                }
+                //查询参保人获取其身份号码
+                person = personDAO.queryByPersonid(personid);
+                if(person == null){
+                    throw new AppException(ResponseStatus.UNKNOWN_ERROR);
+                }
+            }
         }catch (Exception e){
             e.printStackTrace();
             throw new AppException(ResponseStatus.DATA_VALIDATE_EXCEPTION,"数据解析异常");
         }
-        //查询安全级别 或 判断有无此事项
-        String securityLevel = serviceItemDAO.queryByServiceName(serviceName);
-        if(!StringUtils.isEmpty(securityLevel) && securityLevel.equals("3")){ //需要人脸识别的事项
-            //判断用户是否已经人脸识别
-            personid = request.getParameter("personid");
-
-            //如果参数中没有personid 则试用orderno查询personid
-            if(StringUtils.isEmpty(personid)){
-                String orderno = request.getParameter("orderno");
-                Order order = orderDAO.queryByOrderno(orderno);
-                if(order == null){
-                    throw new AppException(ResponseStatus.UNKNOWN_ERROR);
-                }
-                personid = order.getPersonid();
-            }
-            //查询参保人获取其身份号码
-            Person person = personDAO.queryByPersonid(personid);
-            System.out.println(person.getIdcard());
-            if(person == null){
-                throw new AppException(ResponseStatus.UNKNOWN_ERROR);
-            }
-
-            //检验token
-            ValidateFaceService validateFaceService = new ValidateFaceService();
-            validateFaceService.validateToken(request,person.getIdcard());
-        }
+        //检验token
+        ValidateFaceService validateFaceService = new ValidateFaceService();
+        validateFaceService.validateToken(request,person.getIdcard(),person.getName());
         return true;
     }
 
