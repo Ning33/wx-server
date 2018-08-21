26fe051c09d511400d19ff9891750980624bd8b3
fuqiang1996 <315458488@qq.com>
2018-08-21 16:29:54 +0800

    修改JsonUtils类 , 删除 fastJson包

--- src/main/java/cn/hnisi/wx/server/filter/MyWebMvcConfigurerAdapter.java
+++ src/main/java/cn/hnisi/wx/server/filter/MyWebMvcConfigurerAdapter.java
@@ -3,6 +3,7 @@ package cn.hnisi.wx.server.filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 @Configuration
 public class MyWebMvcConfigurerAdapter extends WebMvcConfigurationSupport {
@@ -19,5 +20,17 @@ public class MyWebMvcConfigurerAdapter extends WebMvcConfigurationSupport {
         //excludePathPatterns 用于排除拦截
         registry.addInterceptor(getMyIntercepter()).addPathPatterns("/api/frontend/service/**");
         super.addInterceptors(registry);
+    }
+
+    /**
+     * 访问静态资源
+     * addResourceHandler: 访问的路径
+     * addResourceLocations: 静态资源的位置
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/img");
+        super.addResourceHandlers(registry);
     }
 }
