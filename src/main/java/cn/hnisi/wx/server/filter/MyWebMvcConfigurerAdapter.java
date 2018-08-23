f5bd657934a91fae0feddf873acbdec143516b4f
hnisi\dupengfei <463552890@qq.com>
2018-08-23 11:42:26 +0800

    人脸识别拦截引起其他异常，暂时屏蔽此功能

--- src/main/java/cn/hnisi/wx/server/filter/MyWebMvcConfigurerAdapter.java
+++ src/main/java/cn/hnisi/wx/server/filter/MyWebMvcConfigurerAdapter.java
@@ -1,10 +1,9 @@
 package cn.hnisi.wx.server.filter;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-@Configuration
+
 public class MyWebMvcConfigurerAdapter extends WebMvcConfigurationSupport {
 
     //首先把拦截器进行注册
